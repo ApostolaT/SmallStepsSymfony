97bf8e9c924076694f4fc4f966734d2706c53003
apostola <allex.post26@gmail.com>
2021-04-14 15:27:24 +0300

    enable the use of annotations for routes, add another method in questions controller

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -5,10 +5,24 @@ namespace App\Controller;
 
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class QuestionController
 {
+    /**
+     * @Route("/")
+     * @return Response
+     */
     public function homepage() {
         return new Response('Just a dummy response so we can observe that it works.', 200);
+    }
+
+    /**
+     * @Route("/questions/{slug}")
+     * @param string $slug
+     * @return Response
+     */
+    public function show(string $slug) {
+        return new Response(sprintf('Future page to show the %s question!', ucwords(str_replace('-', ' ', $slug))), 200);
     }
 }
